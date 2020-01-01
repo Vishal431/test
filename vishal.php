ed3fabf7ca5a215e2d54700c940492f5d3456892
Vishal431 <59159912+Vishal431@users.noreply.github.com>
2020-01-01 10:00:03 +0530

    Update vishal.php

--- vishal.php
+++ vishal.php
@@ -7,3 +7,6 @@ sudo: /usr/bin/sudo must be owned by uid 0 and have the setuid bit set
 sudo: error in /etc/sudo.conf, line 0 while loading plugin "sudoers_policy"
 sudo: /usr/lib/sudo/sudoers.so must be only be writable by owner
 sudo: fatal error, unable to load plugins
+
+$ mount -o remount,rw /
+$ chmod 644 /usr/lib/sudo/sudoers.so
